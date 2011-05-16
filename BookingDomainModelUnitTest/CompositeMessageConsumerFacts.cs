674ff5d92390f8b1f3743b483cf17b2c3bcf7473
Mark Seemann <mark@seemann.ms>
2011-05-16 11:40:35 +0200

    Made a unit test generic to reflect the generic nature of the sut.

--- BookingDomainModelUnitTest/CompositeMessageConsumerFacts.cs
+++ BookingDomainModelUnitTest/CompositeMessageConsumerFacts.cs
@@ -1,32 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Xunit.Extensions;
-using Xunit;
-using Ploeh.Samples.Booking.DomainModel;
-using Ploeh.AutoFixture.Xunit;
-using Ploeh.AutoFixture;
 using Moq;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
 
 namespace Ploeh.Samples.Booking.DomainModel.UnitTest
 {
-    public class CompositeMessageConsumerFacts
+    public class ObjectCompositeMessageConsumerFacts : CompositeMessageConsumerFacts<object> { }
+    public class Int32CompositeMessageConsumerFacts : CompositeMessageConsumerFacts<int> { }
+    public class StringCompositeMessageConsumerFacts : CompositeMessageConsumerFacts<string> { }
+    public class VersionCompositeMessageConsumerFacts : CompositeMessageConsumerFacts<Version> { }
+
+    public abstract class CompositeMessageConsumerFacts<T>
     {
         [Theory, AutoDomainData]
-        public void SutIsMessageConsumer(CompositeMessageConsumer<object> sut)
+        public void SutIsMessageConsumer(CompositeMessageConsumer<T> sut)
         {
-            Assert.IsAssignableFrom<IMessageConsumer<object>>(sut);
+            Assert.IsAssignableFrom<IMessageConsumer<T>>(sut);
         }
 
         [Theory, AutoDomainData]
-        public void ConsumersIsCorrect([Frozen]IMessageConsumer<object>[] consumers, CompositeMessageConsumer<object> sut)
+        public void ConsumersIsCorrect([Frozen]IMessageConsumer<T>[] consumers, CompositeMessageConsumer<T> sut)
         {
             Assert.True(consumers.SequenceEqual(sut.Consumers));
         }
 
         [Theory, AutoDomainData]
-        public void ConsumeConsumesAllConsumers(CompositeMessageConsumer<object> sut, object message)
+        public void ConsumeConsumesAllConsumers(CompositeMessageConsumer<T> sut, T message)
         {
             sut.Consume(message);
             var mocks = (from c in sut.Consumers
