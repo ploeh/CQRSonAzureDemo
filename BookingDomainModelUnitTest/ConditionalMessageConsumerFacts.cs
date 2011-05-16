9a874f7c5a4874dfb997321314b714d135b4f109
Mark Seemann <mark@seemann.ms>
2011-05-16 11:58:02 +0200

    Made a unit test generic to reflect the generic nature of the sut.

--- BookingDomainModelUnitTest/ConditionalMessageConsumerFacts.cs
+++ BookingDomainModelUnitTest/ConditionalMessageConsumerFacts.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Xunit.Extensions;
-using Xunit;
-using Ploeh.Samples.Booking.DomainModel;
-using Ploeh.AutoFixture;
-using Ploeh.AutoFixture.Xunit;
 using Moq;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
 
 namespace Ploeh.Samples.Booking.DomainModel.UnitTest
 {
-    public class ConditionalMessageConsumerFacts
+    public class ObjectConditionalMessageConsumerFacts : ConditionalMessageConsumerFacts<object> { }
+    public class Int32ConditionalMessageConsumerFacts : ConditionalMessageConsumerFacts<int> { }
+    public class StringConditionalMessageConsumerFacts : ConditionalMessageConsumerFacts<string> { }
+    public class VersionConditionalMessageConsumerFacts : ConditionalMessageConsumerFacts<Version> { }
+
+    public abstract class ConditionalMessageConsumerFacts<T>
     {
         [Theory, AutoDomainData]
         public void SutIsMessageConsumer(ConditionalMessageConsumer<object> sut)
@@ -20,61 +20,61 @@ namespace Ploeh.Samples.Booking.DomainModel.UnitTest
         }
 
         [Theory, AutoDomainData]
-        public void FirstIsCorrect(IMessageConsumer<object> first, IMessageConsumer<object> second, Func<object, bool> condition)
+        public void FirstIsCorrect(IMessageConsumer<T> first, IMessageConsumer<T> second, Func<T, bool> condition)
         {
-            var sut = ConditionalMessageConsumerFacts.CreateSut(first, second, condition);
-            IMessageConsumer<object> result = sut.First;
+            var sut = ConditionalMessageConsumerFacts<T>.CreateSut(first, second, condition);
+            IMessageConsumer<T> result = sut.First;
             Assert.Equal(first, result);
         }
 
         [Theory, AutoDomainData]
-        public void SecondIsCorrect(IMessageConsumer<object> first, IMessageConsumer<object> second, Func<object, bool> condition)
+        public void SecondIsCorrect(IMessageConsumer<T> first, IMessageConsumer<T> second, Func<T, bool> condition)
         {
-            var sut = ConditionalMessageConsumerFacts.CreateSut(first, second, condition);
-            IMessageConsumer<object> result = sut.Second;
+            var sut = ConditionalMessageConsumerFacts<T>.CreateSut(first, second, condition);
+            IMessageConsumer<T> result = sut.Second;
             Assert.Equal(second, result);
         }
 
         [Theory, AutoDomainData]
-        public void ConditionIsCorrect([Frozen]Func<object, bool> condition, ConditionalMessageConsumer<object> sut)
+        public void ConditionIsCorrect([Frozen]Func<T, bool> condition, ConditionalMessageConsumer<T> sut)
         {
-            Func<object, bool> result = sut.Condition;
+            Func<T, bool> result = sut.Condition;
             Assert.Equal(condition, result);
         }
 
         [Theory, AutoDomainData]
-        public void FirstIsInvokedWhenConditionIsTrue(Mock<IMessageConsumer<object>> firstMock, Mock<IMessageConsumer<object>> secondStub, object message)
+        public void FirstIsInvokedWhenConditionIsTrue(Mock<IMessageConsumer<T>> firstMock, Mock<IMessageConsumer<T>> secondStub, T message)
         {
-            var sut = ConditionalMessageConsumerFacts.CreateSut(firstMock.Object, secondStub.Object, m => true);
+            var sut = ConditionalMessageConsumerFacts<T>.CreateSut(firstMock.Object, secondStub.Object, m => true);
             sut.Consume(message);
             firstMock.Verify(c => c.Consume(message));
         }
 
         [Theory, AutoDomainData]
-        public void SecondIsNotInvokedWhenConditionIsTrue(Mock<IMessageConsumer<object>> firstStub, Mock<IMessageConsumer<object>> secondMock, object message)
+        public void SecondIsNotInvokedWhenConditionIsTrue(Mock<IMessageConsumer<T>> firstStub, Mock<IMessageConsumer<T>> secondMock, T message)
         {
-            var sut = ConditionalMessageConsumerFacts.CreateSut(firstStub.Object, secondMock.Object, m => true);
+            var sut = ConditionalMessageConsumerFacts<T>.CreateSut(firstStub.Object, secondMock.Object, m => true);
             sut.Consume(message);
-            secondMock.Verify(c => c.Consume(It.IsAny<object>()), Times.Never());
+            secondMock.Verify(c => c.Consume(It.IsAny<T>()), Times.Never());
         }
 
         [Theory, AutoDomainData]
-        public void SecondIsInvokedWhenConditionIsFalse(Mock<IMessageConsumer<object>> firstStub, Mock<IMessageConsumer<object>> secondMock, object message)
+        public void SecondIsInvokedWhenConditionIsFalse(Mock<IMessageConsumer<T>> firstStub, Mock<IMessageConsumer<T>> secondMock, T message)
         {
-            var sut = ConditionalMessageConsumerFacts.CreateSut(firstStub.Object, secondMock.Object, m => false);
+            var sut = ConditionalMessageConsumerFacts<T>.CreateSut(firstStub.Object, secondMock.Object, m => false);
             sut.Consume(message);
             secondMock.Verify(c => c.Consume(message));
         }
 
         [Theory, AutoDomainData]
-        public void FirstIsNotInvokedWhenConditionIsFalse(Mock<IMessageConsumer<object>> firstMock, Mock<IMessageConsumer<object>> secondStub, object message)
+        public void FirstIsNotInvokedWhenConditionIsFalse(Mock<IMessageConsumer<T>> firstMock, Mock<IMessageConsumer<T>> secondStub, T message)
         {
-            var sut = ConditionalMessageConsumerFacts.CreateSut(firstMock.Object, secondStub.Object, m => false);
+            var sut = ConditionalMessageConsumerFacts<T>.CreateSut(firstMock.Object, secondStub.Object, m => false);
             sut.Consume(message);
-            firstMock.Verify(c => c.Consume(It.IsAny<object>()), Times.Never());
+            firstMock.Verify(c => c.Consume(It.IsAny<T>()), Times.Never());
         }
 
-        private static ConditionalMessageConsumer<T> CreateSut<T>(IMessageConsumer<T> first, IMessageConsumer<T> second, Func<T, bool> condition)
+        private static ConditionalMessageConsumer<T> CreateSut(IMessageConsumer<T> first, IMessageConsumer<T> second, Func<T, bool> condition)
         {
             return new ConditionalMessageConsumer<T>(condition, first, second);
         }        
--- BookingDomainModelUnitTest/FuncCustomization.cs
+++ BookingDomainModelUnitTest/FuncCustomization.cs
@@ -14,6 +14,9 @@ namespace Ploeh.Samples.Booking.DomainModel.UnitTest
         {
             fixture.Inject<Func<bool>>(() => false);
             fixture.Inject<Func<object, bool>>(x => false);
+            fixture.Inject<Func<string, bool>>(x => false);
+            fixture.Inject<Func<int, bool>>(x => false);
+            fixture.Inject<Func<Version, bool>>(x => false);
         }
 
         #endregion
